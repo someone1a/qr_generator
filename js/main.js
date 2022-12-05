54aae98176584eaee28604c9e685bdbe3186a32b
Walter Carrasco <carrascowalter440@gmail.com>
2022-12-05 18:35:44 -0300

    5/12

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-function generate() {
-	var typeNumber = 4;
-	var errorCorrectionLevel = 'L';
-	var qr = qrcode(typeNumber, errorCorrectionLevel);
-	var inputText = document.getElementById('text').value;
-	qr.addData(inputText);
-	qr.make();
-	document.getElementById('placeHolder').innerHTML = qr.createImgTag();
-	canvasScreen();
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img"),
+descarga = document.querySelector("#descarga"),
+img = document.querySelector("img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generando Codigo QR";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
+
+descarga.addEventListener("click", ()=>{
+
+    let imgPath = img.getAttribute("src");
+    let nombreArchivo = getFileName(imgPath);
+    saveAs(imgPath, nombreArchivo);
+})
+
+function getFileName(str){
+    return str.substr(str.lastIndexOf('/') + 1)
 }
-downloadQrCode = function (el) {
-	var canvas = document.getElementById("myCanvas");
-	var image = canvas.toDataURL("image/png");
-	el.href = image;
-};
-function canvasScreen() {
-	var a = document.getElementsByTagName("img")[0];
-	a.setAttribute("id", "qrcode");
-	var canvas = document.getElementById("myCanvas");
-	var ctx = canvas.getContext("2d");
-	var img = document.getElementById("qrcode");
-	ctx.drawImage(img, 70, 0, 150, 150);
-	document.getElementById("dBtn").style.display = "block";
-}  
